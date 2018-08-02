042a3cb75416c8aecc4cffc9f28ece2ec663f771
trebor <>
2018-08-02 10:18:37 +0800

    create porject

---
+++ WebApi/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello Web API";
+        }
+    }
+}
